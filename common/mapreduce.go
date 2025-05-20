413498c1ef8b205998f8366afcb0e5f8795d5cfd
JedBelhaj <jedbelhaj05@gmail.com>
2025-05-20 18:49:40 +0100

    added reduce

--- common/mapreduce.go
+++ common/mapreduce.go
@@ -38,3 +38,44 @@ func DoMap(jobName string, mapTaskNumber int, inFile string, nReduce int, mapF f
 		file.Close()
 	}
 }
+
+func DoReduce(
+	jobName string,
+	reduceTaskNumber int,
+	nMap int,
+	reduceF func(string, []string) string,
+) {
+	// key -> []string
+	keyValues := make(map[string][]string)
+
+	// 1. READ all intermediate files from each map task
+	for i := 0; i < nMap; i++ {
+		fileName := fmt.Sprintf("mr-%d-%d", i, reduceTaskNumber)
+		file, err := os.Open(fileName)
+		if err != nil {
+			panic(err)
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break // EOF
+			}
+			keyValues[kv.Key] = append(keyValues[kv.Key], kv.Value)
+		}
+		file.Close()
+	}
+
+	// 2. WRITE final output
+	outFileName := fmt.Sprintf("mr-out-%d", reduceTaskNumber)
+	outFile, err := os.Create(outFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+
+	for key, values := range keyValues {
+		result := reduceF(key, values)
+		fmt.Fprintf(outFile, "%v %v\n", key, result)
+	}
+}
--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"mapreduce/common"
+	"strconv"
+	"strings"
 )
 
+func mapF(filename string, contents string) []common.KeyValue {
+	words := strings.Fields(contents)
+	kva := []common.KeyValue{}
+	for _, word := range words {
+		kva = append(kva, common.KeyValue{Key: word, Value: "1"})
+	}
+	return kva
+}
+
+func reduceF(key string, values []string) string {
+	return strconv.Itoa(len(values)) // sum of occurrences
+}
+
 func main() {
-	common.DoMap("job1", 0, "input.txt", 3, common.MapF)
+	// MAP PHASE
+	common.DoMap("job1", 0, "input.txt", 3, mapF)
+
+	// REDUCE PHASE
+	for i := 0; i < 3; i++ {
+		common.DoReduce("job1", i, 1, reduceF)
+	}
 }
